2c2c018fba9031cb25f5859620715c5738812362
Victor Perrin <victor.perrin22@gmail.com>
2025-03-08 14:51:58 +0100

    remove unused files

---
+++ TODO.md
@@ -0,0 +1,1 @@
+* Ajouter une section training (Mindshare)
---
+++ docker_run.sh
@@ -0,0 +1,10 @@
+docker run --rm \
+  -it \
+  --entrypoint /bin/bash \
+  -v /$(pwd)://work \
+  -w //work \
+  danteev/texlive
+
+  # thomasweise/texlive
+
+  # mfisherman/texlive
